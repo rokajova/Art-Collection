4712707c5f75dc2ef10dd78f86856e9ff057e987
rokajova <35878122+rokajova@users.noreply.github.com>
2021-08-07 20:48:50 +0300

    Carousel

--- artcollection/pages/index.js
+++ artcollection/pages/index.js
@@ -1,20 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-     <h1> COLLAPSIBLE</h1>
+      <h1> COLLAPSIBLE</h1>
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-         Ai art
+          Ai art
         </a>
       </footer>
     </div>
-  )
+  );
 }
