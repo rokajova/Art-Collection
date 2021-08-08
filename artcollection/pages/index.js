ecaafacbafd836afaa1200a6ee0688703d01c161
rokajova <35878122+rokajova@users.noreply.github.com>
2021-08-08 14:50:25 +0300

    Carousler image slider GOOD

--- artcollection/pages/index.js
+++ artcollection/pages/index.js
@@ -1,20 +1,34 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
 
 export default function Home() {
+  const images = [
+    {
+      original: "/1.png",
+      thumbnail: "https://picsum.photos/id/1018/250/150/",
+    },
+    {
+      original: "/2.png",
+      thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+      original: "3.png",
+    },
+  ];
+
   return (
-    <div className={styles.container}>
-      <h1> COLLAPSIBLE</h1>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ai art
-        </a>
-      </footer>
+    <div>
+      <nav>Pirate</nav>
+      <ImageGallery
+        showIndex={true}
+        indexSeparator={" | "}
+        showThumbnails={false}
+        items={images}
+      />
+      <div>SHOW ALL</div>
     </div>
   );
 }
