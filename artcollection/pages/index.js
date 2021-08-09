a33d889167a3bbf44f540640516a2fe4bc823677
rokajova <35878122+rokajova@users.noreply.github.com>
2021-08-09 23:43:52 +0300

    CSS MODULES/HOME PAGE/COLLECTION PAGE

--- artcollection/pages/index.js
+++ artcollection/pages/index.js
@@ -1,8 +1,26 @@
 import Head from "next/head";
 import Image from "next/image";
-import ImageGallery from "react-image-gallery";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 import "react-image-gallery/styles/css/image-gallery.css";
 
 export default function Home() {
-  return <div>HOME</div>;
+  return (
+    <div className={styles.container}>
+      <h2>/ pirate soul island / collection</h2>
+      <Link href="/pirate-soul-island">
+        <Image
+          src="/psi.png"
+          height="400"
+          width="500"
+          className={styles.image}
+        />
+      </Link>
+      <Link href="https://opensea.io/collection/pirate-soul-island">
+        <a target="_blank" className={styles.button}>
+          Explore on Opensea!
+        </a>
+      </Link>
+    </div>
+  );
 }
--- artcollection/pages/pirate-soul-island.js
+++ artcollection/pages/pirate-soul-island.js
@@ -1,33 +1,23 @@
 import Head from "next/head";
-import Image from "next/image";
 import { useState, useEffect } from "react";
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 import firebase from "../config/firebase";
 import "firebase/storage";
+import styles from "../styles/Collection.module.css";
 
 export default function Home() {
   const [art, setArt] = useState([]);
 
   const [original, setOriginal] = useState("");
   const [description, setDescription] = useState("");
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    firebase.firestore().collection("Art").add({
-      original: original,
-      description: description,
-    });
-
-    setOriginal("");
-    setDescription("");
-  };
+  const [lore, setLore] = useState("");
 
   useEffect(() => {
     firebase
       .firestore()
       .collection("Art")
+      .orderBy("number")
       .onSnapshot((snap) => {
         const art = snap.docs.map((doc) => ({
           ...doc.data(),
@@ -40,26 +30,23 @@ export default function Home() {
     <div>
       <div style={{ borderBottom: "1px solid #313131" }}>
         {" "}
-        <ImageGallery showIndex={true} indexSeparator={" | "} items={art} />
+        <ImageGallery
+          showIndex={true}
+          indexSeparator={" | "}
+          items={art}
+          slideDuration={600}
+          slideInterval={5000}
+        />
       </div>
 
-      <form>
-        <input
-          type="text"
-          placeholder="original link"
-          value={original}
-          onChange={({ target }) => setOriginal(target.value)}
-        />
-        <input
-          type="text"
-          placeholder="description"
-          value={description}
-          onChange={({ target }) => setDescription(target.value)}
-        />
-        <button onClick={handleSubmit}>GET THAT SHIT IN THE DB</button>
-      </form>
-
-      <button onClick={() => console.log(art)}>Check art</button>
+      {art.map((res) => (
+        <div className={styles.container}>
+          {" "}
+          <h3>{res.description}</h3>
+          <img src={res.original} />
+          <p>{res.lore}</p>
+        </div>
+      ))}
     </div>
   );
 }
