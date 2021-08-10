ffd90df951908a3bf77213c8a3fbe9b63d46f1f5
rokajova <35878122+rokajova@users.noreply.github.com>
2021-08-10 20:52:29 +0300

    Vercel deployment fix

--- Components/Layout.js
+++ Components/Layout.js
@@ -1,5 +1,5 @@
-import Navbar from "./Navbar";
-import Footer from "./Footer";
+import Navbar from "./Navbar.js";
+import Footer from "./Footer.js";
 
 const Layout = ({ children }) => {
   return (
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import Layout from "../components/Layout";
+import Layout from "../components/Layout.js";
 
 function MyApp({ Component, pageProps }) {
   return (
