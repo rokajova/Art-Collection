7a1a4bfe906bb755de71ab54ceda879bfab2cfb6
rokajova <35878122+rokajova@users.noreply.github.com>
2021-08-10 21:17:46 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import Layout from "../Components/Layout.js";
+// https://github.com/vercel/next.js/discussions/18176 - CHECK NAMING DISCREPANCIES NEXT TIME YOU DUMBASS. ALL IMPORTS MUST MATCH. THEN YOU WONT HAVE TO WASTE 2 HOURS ON THIS
 
 function MyApp({ Component, pageProps }) {
   return (
